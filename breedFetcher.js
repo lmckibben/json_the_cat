e39d66599b22c776e9de6a9850dd1543948615d1
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-26 20:07:56 +0000

    refactored code, making better function names/ and fectcher into own file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,7 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const cat = args[0];
 
-const fetchCat = () => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${cat}`, (error, response ,body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response ,body) => {
     const data = JSON.parse(body);
     // console.log(data);
     // console.log(typeof data);
@@ -16,4 +14,4 @@ const fetchCat = () => {
   });
 };
 
-fetchCat();
+module.exports = { fetchBreedDescription };
